6c6d50f080b974522bf1742e1c3aad791f7e164d
HoonE <66302392+SiHoon61@users.noreply.github.com>
2024-04-15 19:13:37 +0900

    [level 1] Title: 과일 장수, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -1,8 +1,6 @@
 function solution(k, m, score) {
     var answer = 0;
-    var sum = 0;
     score.sort((a, b)=> b - a);
-    var num = score.length/m;
     var count = 0;
     for(i = 0 ; i < score.length ; i++) {
         if((i + 1) % m === 0) {
