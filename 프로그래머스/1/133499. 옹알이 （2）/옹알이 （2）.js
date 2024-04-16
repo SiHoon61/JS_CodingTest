fed582b5774adf8c0787a09abb29d4c50c898f3d
HoonE <66302392+SiHoon61@users.noreply.github.com>
2024-04-16 17:09:05 +0900

    [level 1] Title: 옹알이 (2), Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
@@ -1,6 +1,5 @@
 function solution(babbling) {
     var answer = 0;
-    var str = [];
     var before = 0;
     for(i = 0 ; i < babbling.length ; i++) {
         var before = 0;
@@ -8,22 +7,18 @@ function solution(babbling) {
             if(babbling[i].slice(0, 3) === 'aya' && before !== 1){
                 before = 1;
                 babbling[i] = babbling[i].substr(3);
-                console.log('1', babbling[i]);
             }
             else if(babbling[i].slice(0, 2) === 'ye' && before !== 2){
                 before = 2;
                 babbling[i] = babbling[i].substr(2);
-                console.log('2', babbling[i]);
             }
             else if(babbling[i].slice(0, 3) === 'woo' && before !== 3){
                 before = 3;
                 babbling[i] = babbling[i].substr(3);
-                console.log('3', babbling[i]);
             }
             else if(babbling[i].slice(0, 2) === 'ma' && before !== 4){
                 before = 4;
                 babbling[i] = babbling[i].substr(2);
-                console.log('4', babbling[i]);
             }
             else{
                 break;
