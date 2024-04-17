aa26cb10e074dbdb251b27a51e405557bbe19eee
HoonE <66302392+SiHoon61@users.noreply.github.com>
2024-04-17 13:35:26 +0900

    [level 1] Title: 삼총사, Time: 0.15 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -0,0 +1,22 @@
+function solution(number) {
+    var answer = 0;
+    var sum = 0;
+    for(i = 0 ; i < number.length; i++){
+        for(k = i + 1 ; k < number.length; k++) {
+            for(p = k + 1 ; p < number.length; p++) {
+                sum = number[i] + number[k] + number[p]
+                if(sum === 0) {
+                    answer++;
+                } 
+            }
+        }
+    }
+    return answer;
+}
+
+012
+013
+014
+023
+024
+034
