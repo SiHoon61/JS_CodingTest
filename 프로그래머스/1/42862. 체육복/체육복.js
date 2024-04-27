61ade60a23cadb5aea5315ffb8bf1267493db82d
HoonE <66302392+SiHoon61@users.noreply.github.com>
2024-04-27 22:26:37 +0900

    [level 1] Title: 체육복, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -0,0 +1,55 @@
+function solution(n, lost, reserve) {
+    var answer = 0;
+    let index = 0;
+    for(i = 0 ; i < lost.length ; i++) {
+        index = reserve.indexOf(lost[i]);
+        if(index != -1){
+            reserve.splice(index, 1);
+            lost[i] = -10;
+        }
+    }
+    let next_index = 0;
+    for(i = 0 ; i < reserve.length ; i++) {
+        index = lost.indexOf(reserve[i] - 1);
+        next_index = lost.indexOf(reserve[i] + 1)
+        if((lost[next_index] - 2) != lost[index]){
+            if(index != -1){
+                lost.splice(index, 1);
+                reserve[i] = NaN;
+            }
+        }
+    }
+    
+    for(i = (reserve.length - 1) ; i >= 0 ; i--) {
+        index = lost.indexOf(reserve[i] + 1);
+        next_index = lost.indexOf(reserve[i] - 1)
+        if((lost[next_index] + 2) != lost[index]){
+            if(index != -1){
+                lost.splice(index, 1);
+                reserve[i] = NaN;
+            }
+        }
+    }
+    
+    for(i = 0 ; i < reserve.length ; i++) {
+        index = lost.indexOf(reserve[i] - 1);
+        if(index != -1){
+            lost.splice(index, 1);
+            reserve[i] = NaN;
+        }
+    }
+    
+    for(i = (reserve.length - 1) ; i >= 0 ; i--) {
+        index = lost.indexOf(reserve[i] + 1);
+        if(index != -1){
+            lost.splice(index, 1);
+             reserve[i] = NaN;
+        }
+    }
+    for(i = 0 ; i < lost.length ; i++) {
+        if(lost[i] != -10){
+            n--;
+        }
+    }
+    return n;
+}
