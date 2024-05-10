485419ca382ad229019c2374968e9297c7a21f60
HoonE <66302392+SiHoon61@users.noreply.github.com>
2024-05-10 17:38:16 +0900

    [level 1] Title: 문자열을 정수로 바꾸기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.js
+++ 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.js
@@ -1,12 +1,3 @@
 function solution(s) {
-    var answer = 1;
-    if(s[0] === '+'){
-        [...s].shift();
-    }
-    else if(s[0] === '-'){
-        [...s].shift();
-    }
-    console.log(s);
-    
     return (+s);
 }
